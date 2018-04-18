2ac28d64815abf7e1b4889a5112ad7b57e82784b
bastioncillo <33004217+bastioncillo@users.noreply.github.com>
2018-04-18 22:27:02 +0200

    A bit of Mario

--- practica3/src/practica3.js
+++ practica3/src/practica3.js
@@ -6,17 +6,40 @@ var game = function() {
 					height:480})
 			.controls().touch();
 
-
+	//--------------LEVEL-----------------
 	Q.scene("level1",function(stage) {
 		Q.stageTMX("level.tmx",stage);
-		stage.add("viewport");
 		stage.centerOn(150, 380);
+		var mario = stage.insert(new Mario());
+		stage.add("viewport").follow(mario);
 	});
 
 	Q.loadTMX('level.tmx', function() {
 		Q.stageScene("level1");
 	});
 
+	//-------------MARIO----------------
+	Q.load(["mario_small.png","mario_small.json"], function(){
+		Q.compileSheets("mario_small.png","mario_small.json");
+	});
+
+	Q.Sprite.extend("Mario", {
+		init:function(p){
+			this._super({
+				sheet: "mario_small",
+				frame: 1
+			});
+			this.add('2d, plataformerControls');
+			this.on("bump.botton", this, "stomp");
+		},
+
+		stomp: function(collision){
+			if(collision.obj.isA("Goomba")){
+				collision.obj.destroy();
+				this.p.vy = -500; //mkae Mario jump
+			}
+		}
+	});
 
 }
 
