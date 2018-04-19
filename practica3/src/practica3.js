e81f34d8d654623a69c760d675ec3176dc0db10b
bastioncillo <33004217+bastioncillo@users.noreply.github.com>
2018-04-19 15:51:08 +0200

    Mario fixed

--- practica3/src/practica3.js
+++ practica3/src/practica3.js
@@ -9,27 +9,32 @@ var game = function() {
 	//--------------LEVEL-----------------
 	Q.scene("level1",function(stage) {
 		Q.stageTMX("level.tmx",stage);
-		stage.centerOn(150, 380);
-		var mario = stage.insert(new Mario());
+		var mario = stage.insert(new Q.Mario());
 		stage.add("viewport").follow(mario);
+		stage.viewport.offsetX = -130;
+		stage.viewport.offsetY = 156;
+		stage.centerOn(150, 380);
 	});
 
-	Q.loadTMX('level.tmx', function() {
-		Q.stageScene("level1");
-	});
+
 
 	//-------------MARIO----------------
 	Q.load(["mario_small.png","mario_small.json"], function(){
 		Q.compileSheets("mario_small.png","mario_small.json");
+		Q.loadTMX('level.tmx', function() {
+			Q.stageScene("level1");
+		});
 	});
 
 	Q.Sprite.extend("Mario", {
 		init:function(p){
-			this._super({
-				sheet: "mario_small",
-				frame: 1
+			this._super(p,{
+				sheet: "mario",
+				frame: 1,
+				x: 30,
+				y:380
 			});
-			this.add('2d, plataformerControls');
+			this.add('2d, platformerControls');
 			this.on("bump.botton", this, "stomp");
 		},
 
@@ -37,6 +42,15 @@ var game = function() {
 			if(collision.obj.isA("Goomba")){
 				collision.obj.destroy();
 				this.p.vy = -500; //mkae Mario jump
+			}
+		},
+
+		step: function(){
+			if(this.p.y >= 800){
+				this.p.sheet = "mario";
+				this.p.frame = 1;
+				this.p.x = 30;
+				this.p.y = 380;
 			}
 		}
 	});
