75f1620dabef6e1d481928992d4ba56231b8a208
JavierCarrion <jacarrio@ucm.es>
2018-04-20 12:44:19 +0200

    Added enemies, collisions and the end game window

--- practica3/src/practica3.js
+++ practica3/src/practica3.js
@@ -10,6 +10,9 @@ var game = function() {
 	Q.scene("level1",function(stage) {
 		Q.stageTMX("level.tmx",stage);
 		var mario = stage.insert(new Q.Mario());
+		var goomba = stage.insert(new Q.Goomba());
+		var bloopa = stage.insert(new Q.Bloopa());
+
 		stage.add("viewport").follow(mario);
 		stage.viewport.offsetX = -130;
 		stage.viewport.offsetY = 156;
@@ -17,43 +20,132 @@ var game = function() {
 	});
 
 
+	Q.scene("endGame",function(stage){
+		var container = stage.insert(new Q.UI.Container({
+			x: Q.width/2, y: Q.height/2 , fill: "rgba(0,0,0,0.5)"
+		}));
+		var button = container.insert(new Q.UI.Button({x:0, y:0, fill: "#CCCCCC", label: "Play Again"}))
+		var label = container.insert(new Q.UI.Text({x:10, y:-10 - button.p.h, label: stage.options.label}));
+		button.on("click", function(){
+			Q.clearStages();
+			Q.stageScene("level1");
+		});
+		container.fit(20);
+	});
 
-	//-------------MARIO----------------
-	Q.load(["mario_small.png","mario_small.json"], function(){
+	
+	Q.load(["mario_small.png","mario_small.json",
+					"goomba.png", "goomba.json", 
+					"bloopa.png", "bloopa.json"], function(){
+
 		Q.compileSheets("mario_small.png","mario_small.json");
+		Q.compileSheets("goomba.png", "goomba.json");
+		Q.compileSheets("bloopa.png", "bloopa.json");
+
 		Q.loadTMX('level.tmx', function() {
 			Q.stageScene("level1");
 		});
 	});
 
+
+
+	//-------------MARIO----------------
 	Q.Sprite.extend("Mario", {
 		init:function(p){
 			this._super(p,{
 				sheet: "mario",
-				frame: 1,
+				frame: 0,
 				x: 30,
 				y:380
 			});
 			this.add('2d, platformerControls');
-			this.on("bump.botton", this, "stomp");
-		},
-
-		stomp: function(collision){
-			if(collision.obj.isA("Goomba")){
-				collision.obj.destroy();
-				this.p.vy = -500; //mkae Mario jump
-			}
 		},
 
 		step: function(){
 			if(this.p.y >= 800){
 				this.p.sheet = "mario";
-				this.p.frame = 1;
-				this.p.x = 30;
-				this.p.y = 380;
+				this.destroy();
+				Q.stageScene("endGame", 1, {label: "You Died"});
 			}
 		}
 	});
 
-}
 
+
+//-------------GOOMBA----------------
+
+	Q.Sprite.extend("Goomba", {
+		init:function(p){
+			this._super(p,{
+				sheet: "goomba",
+				frame: 0,
+				x: 1500,
+				y:380,
+				vx: 80
+			});
+			this.add('2d, aiBounce');
+			this.on("bump.left, bump.right, bump.bottom", this, "stomp");
+			this.on("bump.top",this, "smash");
+		},
+
+		smash: function(collision){
+			if(collision.obj.isA("Mario")){
+				this.destroy();
+				collision.obj.p.vy = -300;
+			}
+		},
+
+		stomp: function(collision){
+			if(collision.obj.isA("Mario")){
+				collision.obj.destroy();
+				Q.stageScene("endGame", 1, {label: "You Died"});
+				//collision.obj.p.sheet = "mario";
+				//collision.obj.p.frame = 0;
+				//collision.obj.p.x = 30;
+				//collision.obj.p.y = 380;
+			}
+		}
+	});
+
+
+//-------------BLOOPA----------------
+
+	Q.Sprite.extend("Bloopa", {
+		init:function(p){
+			this._super(p,{
+				sheet: "bloopa",
+				frame: 0,
+				x: 1150,
+				y:380,
+				vy: -350,
+				gravityY: 5*100
+			});
+			this.add('2d');
+			this.on("bump.left, bump.right, bump.bottom", this, "stomp");
+			this.on("bump.top",this, "smash");
+		},
+		step: function(){
+			if(this.p.y >= 519){
+				this.p.vy = -350;
+			}
+		},
+
+		smash: function(collision){
+			if(collision.obj.isA("Mario")){
+				this.destroy();
+				collision.obj.p.vy = -300;
+			}
+		},
+
+		stomp: function(collision){
+			if(collision.obj.isA("Mario")){
+				collision.obj.destroy();
+				Q.stageScene("endGame", 1, {label: "You Died"});
+				//collision.obj.p.sheet = "mario";
+				//collision.obj.p.frame = 0;
+				//collision.obj.p.x = 30;
+				//collision.obj.p.y = 380;
+			}
+		}
+	});
+}
